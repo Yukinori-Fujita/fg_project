4278499a41706b346073dd32bd19bbe4dea3b04e
藤田 幸法 <straight.0429@gmail.com>
2022-06-04 21:05:15 +0900

    JQueryでタイプライター作れなかったので、これだけjsで行きましょう！

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// タイプライター
+// jsの場合
 const typewriter = (param) => {
     let el = document.querySelector(param.el);
     let speed = param.speed;
@@ -14,5 +16,29 @@ typewriter({
     el: "#typewriter",
     speed: 100,
     string: "I AM YUKINORI FUJITA. I AM YUNA GUSHIKEN. FG_PROJECT START !",
-    string: "I AM YUKINORI FUJITA. I AM YUNA GUSHIKEN. FG_PROJECT START !",
 });
+// jqueryの場合
+// $(function(){
+//         var setElm = $('.split'),
+//         delaySpeed = 200,
+//         fadeSpeed = 0;
+//         setText = setElm.html();
+//         setElm.css({visibility:'visible'}).children().addBack().contents().each(function(){
+//             var elmThis = $(this);
+//             if (this.nodeType == 3) {
+//                 var $this = $(this);
+//                 $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+//             }
+//         });
+//         $(window).load(function(){
+//             splitLength = $('.textSplitLoad').length;
+//             setElm.find('.textSplitLoad').each(function(i){
+//                 splitThis = $(this);
+//                 splitTxt = splitThis.text();
+//                 splitThis.delay(i*(delaySpeed)).css({display:'inline-block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+//             });
+//             setTimeout(function(){
+//                     setElm.html(setText);
+//             },splitLength*delaySpeed+fadeSpeed);
+//         });
+//     });
