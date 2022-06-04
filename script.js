43e5226e012e98602ccc938dbab7edda2afddede
ggg-yuna <gushikenyuna@gmail.com>
2022-06-05 06:58:32 +0900

    あ、間違えた

---
+++ script.js
@@ -0,0 +1,43 @@
+$(function (){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ページトップに戻る
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function (){
+        if($(this).scrollTop() > 100){
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    })
+    pagetop.click(function (){
+        $('body,html').animate({ scrollTop: 0 },500 );
+        return false;
+    })
+    
+
+
+
+
+
+
+
+
+
+    // オオワクノカッコ
+})
