9cfc33869b55f39f908f8dc54ea06a3875086757
ggg-yuna <gushikenyuna@gmail.com>
2022-06-06 13:22:38 +0900

    ゆるキャラhover追加（とりあえずjqery使っとく）

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 $(function () {
 
-    
+    // ゆるキャラhover
+    $('.Item').each(function() {
+
+        let img_off = $(this).attr('src');
+        let img_on = $(this).attr('src').replace('off', 'on');
+      
+        $(this).hover(
+          function () {
+            $(this).attr('src', img_on);
+          },
+          function () {
+            $(this).attr('src', img_off);
+          }
+        );
+      
+      });
+
+
+
     // JSの場合
     const typewriter = (param) => {
         let el = document.querySelector(param.el);
